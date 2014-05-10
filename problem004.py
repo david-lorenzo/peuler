e0261ea5b83aa42a41c2eebf6504878d78975b26
David Lorenzo <david.lorenzo.nunez@gmail.com>
2014-05-10 11:41:36 -0700

    Optimization
    Added an iteration counter
    Added a lower bound when we try to find a divisor

--- problem004.py
+++ problem004.py
@@ -1,5 +1,7 @@
 #http://projecteuler.net/problem=4
 
+from math import sqrt
+
 class palindrome :
     def __init__(self) :
         self.i = 9
@@ -31,18 +33,22 @@ class palindrome :
         self.decrement()
         return r
             
+iterations = 0
 def divisible_p(n) :
-    for i in range(999,99,-1) :
+    global iterations
+    for i in range(999, int(sqrt(n))-1,-1) :
+        iterations +=1
         if (n % i == 0) : return True, i, n/i
     return False, None, None
 
 def p4():
+    global iterations
     p = palindrome()
     r = []
     n = p.next()
     while n :
         test, d1, d2 = divisible_p(n)
-        if test and (int(d2/1000) == 0): return n, d1, d2, d1*d2
+        if test and (int(d2/1000) == 0): return n, d1, d2, d1*d2, iterations
         n = p.next()
     return None, None, None, None
 
